cf36e2a712f8ac1c7afd01ed9b0b21d5651cefc9
tachiyamama <tachiyama.megumi@gmail.com>
2023-03-03 11:47:07 +0900

    uniquness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
 
   #@以降の.連結を有効にしてしまう
@@ -6,5 +7,6 @@ class User < ApplicationRecord
   # @以降の.連結も無効とする
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX }
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
 end
