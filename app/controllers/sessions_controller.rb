743c49eb48b20289f0c6b0075ef460ed94b925e0
tachiyamama <tachiyama.megumi@gmail.com>
2023-03-06 11:34:20 +0900

    toggle menu js

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #ログイン後のリダイレクト
-
+      reset_session      # ログインの直前に必ずこれを書くこと
+      log_in user
+      redirect_to user
     else
       #error
       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
