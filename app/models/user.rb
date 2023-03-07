3af82724bc80685eb613161d5b9f5737e91c158f
tachiyamama <tachiyama.megumi@gmail.com>
2023-03-07 09:06:16 +0900

    newline test

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,18 +14,19 @@ class User < ApplicationRecord
   has_secure_password
   #validates :password, presence: true, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  class << self
-    # 渡された文字列のハッシュ値を返す
-    def digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-               BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
-    # ランダムなトークンを返す
-    def new_token
-      SecureRandom.urlsafe_base64
-    end
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
   # 永続セッションのためにユーザーをデータベースに記憶する
   def remember
     self.remember_token = User.new_token
